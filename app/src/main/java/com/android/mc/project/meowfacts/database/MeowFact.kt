fff5d2dab951bbb4605dbd3d398ba6cb26b0fc8e
Alexandru <alexandru.ionita99@stud.acs.upb.ro>
2023-01-02 10:31:24 +0200

    Room WIP #4

--- app/src/main/java/com/android/mc/project/meowfacts/database/MeowFact.kt
+++ app/src/main/java/com/android/mc/project/meowfacts/database/MeowFact.kt
@@ -6,6 +6,8 @@ import com.squareup.moshi.Json
 
 @Entity(tableName = "meow_facts_table")
 data class MeowFact(
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
+    val factId: Long = 0L,
+    @ColumnInfo(name = "fact")
     val fact: String = ""
 )
